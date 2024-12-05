d6443390bc4746ccedb530c1a42d3eef8d018abc
Lucas <lucas@yesbabylon.com>
2024-12-05 09:14:40 +0100

    add vsftpd.conf and base for backup host install

--- tapu_backups/install.sh
+++ tapu_backups/install.sh
@@ -1,5 +1,73 @@
 #!/bin/bash
 
+# Default values
+BACKUP_DISK="/dev/sdb"
+
+# Function to display help
+flags_help() {
+    echo "Usage: script.sh [options]"
+    echo "Options:"
+    echo "  --backup_disk, -w <path>  Specify backups disk name."
+    echo "  --help, -h                Show help message."
+    exit 0
+}
+
+# Parse options
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --backup_disk|-w )
+            BACKUP_DISK="$2"
+            if [[ -z "BACKUP_DISK" ]]; then
+                echo "Error: --backup_disk requires a value."
+                exit 1
+            fi
+            shift ;;  # Skip the value
+        --help|-h )
+            flags_help ;;
+        * )
+            echo "Unknown option: $1"
+            flags_help ; exit 1 ;;
+    esac
+    shift
+done
+
+
+##########################
+### Mount /mnt/backups ###
+##########################
+
+# Create backups directory
+mkdir /mnt/backups
+
+# Format disk to ext filesystem
+mkfs -t ext4 $BACKUP_DISK
+
+# Handle auto mount on startup
+echo "$BACKUP_DISK	/mnt/backups	ext4	defaults	0	0" >> /etc/fstab
+
+# Mount disk
+mount /mnt/backups
+
+
+##########################
+### Install ftp server ###
+##########################
+
+# Install ftp service
+apt-get install -y vsftpd
+
+# Custom FTP config
+mv /etc/vsftpd.conf /etc/vsftpd.conf.orig
+cp "$INSTALL_DIR"/conf/etc/vsftpd.conf /etc/vsftpd.conf
+
+# Restart FTP service
+systemctl restart vsftpd
+
+
+########################
+### Install listener ###
+########################
+
 # Add a symbolic link for the eQual instance listener service
 ln -s /root/aru/tapu/host-backups-listener.service /etc/systemd/system/host-backups-listener.service
 
