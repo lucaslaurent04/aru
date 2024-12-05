0722d17a7b508827f055a3517e84de3734be985d
Lucas <lucas@yesbabylon.com>
2024-12-05 11:24:02 +0100

    enhance list of backups for an instance endpoint

--- tapu_backups/controllers/instance/backups.php
+++ tapu_backups/controllers/instance/backups.php
@@ -5,13 +5,17 @@
  *
  * @param array{instance: string} $data
  * @return array{code: int, body: string[]}
+ * @throws Exception
  */
 function instance_backups(array $data): array {
     // Retrieve the list files contained in a folder
     $backups = scandir('/home/backups/'.$data['instance']);
+    if($backups === false) {
+        throw new Exception("backups_not_found", 404);
+    }
 
     // Remove the '.' and '..' and 'ubuntu' and 'docker' entries
-    $backups = array_diff($backups, ['.', '..', 'ubuntu', 'docker']);
+    $backups = array_values(array_diff($backups, ['.', '..', 'ubuntu', 'docker']));
 
     return [
         'code' => 200,
