ac7f02a1a3b24b1973694262ac70adbed113ad9d
Lucas <lucas@yesbabylon.com>
2024-12-06 10:06:57 +0100

    log error to find why token create doesn't work

--- tapu_backups/listener.php
+++ tapu_backups/listener.php
@@ -82,6 +82,8 @@ try {
     // Respond with the returned body and code
     ['body' => $body, 'code' => $code] = $handler_method_name($data);
 } catch (Exception $e) {
+    file_put_contents(BASE_DIR . '/tmp.log', $e->getMessage());
+
     // Respond with the exception message and status code
     [$body, $code] = [$e->getMessage(), $e->getCode()];
 }
